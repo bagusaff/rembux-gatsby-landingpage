2fba3f8f7e8807533b28e7df48aa0868c6c12854
bagusaff <fkrfajri@gmail.com>
2021-05-25 19:15:06 +0700

    edit description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Rembux Softwarehouse`,
+    description: `Welcome to Rembux softwarehouse, Create the dream app that you need. Worrying about price ? set the price yourself !`,
+    author: `@rembux.dev`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
